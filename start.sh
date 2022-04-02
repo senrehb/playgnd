f996fa3bfb9cc573a9050650ddf54dea77da4a31
senrehb <88944254+senrehb@users.noreply.github.com>
2022-04-02 12:07:08 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 # check exsit to open data
-if [ -f /home/pi/playgnd/read-dht21.py ]
+if [ -f /home/pi/playgnd/read-dht22.py ]
         then
-        echo "Datei vorhanden"
+        echo "... Datei-Prüfung erfolgreich"
         echo
         echo "Sensorabfrage gestartet..."
         echo 
       # cd /home/pi/playgnd/
         python read-dht22.py
+        # // RM script
+        echo
+        echo "Sensorabfrage beendet"
+        echo
         sleep 5
         else
-        echo " Fehler - Datei nicht vorhanden"
+        echo
+        echo " Fehler - Die Datei ist nicht vorhanden"
+        echo
 fi
 echo "The End"
