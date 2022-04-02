0bff7d814aa088b397d3bd6fcc5f9a047c3c11fa
senrehb <88944254+senrehb@users.noreply.github.com>
2022-04-02 10:41:02 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,14 @@ for ((i=1; i<=1440; i++)); do
         echo
         echo "Sensorabfrage gestartet..."
         echo ""
-# Verzeichnis Script
-#        cd /home/pi/
- #       python read-dht22.py
+        cd /home/pi/playgnd/
+       python read-dht22.py
+       IF NOT errorlevel 1 goto End
         echo "...OK."
         sleep 5
 done
+:End
+echo
+echo "Fehler !"
+sleep 5
 # End
