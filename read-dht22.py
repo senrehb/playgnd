675580a30589409e59fa522b25d35cb9a1be6a59
senrehb <88944254+senrehb@users.noreply.github.com>
2022-04-02 12:44:27 +0200

    Update read-dht22.py

--- read-dht22.py
+++ read-dht22.py
@@ -1,35 +1,27 @@
-import time
-#import board
+# Bibliothek für Sensor importieren
+# Bibliothek für Datum Uhrzeit
 import Adafruit_DHT
- 
-# Initial the dht device, with data pin connected to:
-# dhtDevice = adafruit_dht.DHT22(board.D4)
- 
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
-#DHT_SENSOR = Adafruit_DHT.DHT22
- 
-while True:
-    try:
-        # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
-        )
- 
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
- 
-    time.sleep(2.0)
+import time
+#Sensortyp und GPIO festlegen
+sensor = Adafruit_DHT.DHT22
+gpio = 4
+# Daten auslesen
+humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+# Ausgabe
+print 'Temperatur(C): {0:0.1f} Luftfeuchtigkeit(%): {1:0.1f}'.format(temperature,humidity)
+# Ausgabe Datei
+# lokale Datein oeffnen
+f = open('sensor1.log', 'a')
+# Werte formatieren
+valueDT = time.strftime("%d.%m.%Y;%H:%M:%S; ")
+value1 =  'Temp: {0:0.1f} ; Luftfeuchte: {1:0.1f}'.format(temperature,humidity)
+value2= '\n'
+# Werte in in Variable schreiben
+s0 = str(valueDT)
+s1 = str(value1)
+s2 = str(value2)
+# Variablen zusamensetzen
+s = s0 + s1 + s2
+# Ausgabe in geöffnete Datei
+f.write(s)
+# Ende
