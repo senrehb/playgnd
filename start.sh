d4d814bdb211adcfc962e7c297ad97d583ad3956
senrehb <88944254+senrehb@users.noreply.github.com>
2022-04-14 19:42:48 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ if [ -f /home/pi/projekt/playgnd/read-dht22.py ]
       # cd /home/pi/playgnd/
         python read-dht22.py 22 4
         # // RM script
-                if [ $1 = '1' ]
+                if $1 = 1
                 then
                echo
                 echo "Fehler Sensorabfrage"
