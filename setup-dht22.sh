8f280b8554e19fcf32c5804c7c3da0cfbce82c6c
senrehb <88944254+senrehb@users.noreply.github.com>
2024-01-03 19:16:28 +0100

    Update and rename setup.py to setup-dht22.sh

---
+++ setup-dht22.sh
@@ -0,0 +1,12 @@
+#ooooooooooooooooooooooooooooooooooooooooooooooooo
+#ooxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxoo
+#oox  playgnd:setup.sh                         xoo
+#oox        version: 0.1 (alpha)               xoo
+#oox            by : senrehb                   xoo
+#ooxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxoo
+#ooooooooooooooooooooooooooooooooooooooooooooooooo
+#  comment:  add dht22 support to python  
+#
+# apt-get install build-essential python-dev python-openssl git
+# sudo pip3 install adafruit-circuitpython-dht
+# apt-get install libgpiod2
--- setup.py
+++
@@ -1,3 +0,0 @@
-# apt-get install build-essential python-dev python-openssl git
-# sudo pip3 install adafruit-circuitpython-dht
-# apt-get install libgpiod2
